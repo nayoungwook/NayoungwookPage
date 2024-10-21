c4d459b708742cf7628060f2b66fa9a8259b41b1
nayoungwook <nayoungwoook@gmail.com>
2024-10-21 23:29:20 +0900

    blog in dev.

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
-const { join } = require('path');
 const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const fs = require('fs');
 
 const PORT = 80;
+
+const PSW = "Sushiroll8016!";
+const KEY = 'Sushiroll8016!';
 
 app.use(express.static('.'));
 
@@ -12,6 +19,40 @@ app.get('/', (req, res) => {
 
 app.get('/admin', (req, res) => {
     res.sendFile(join(__dirname, '/pages/admin.html'));
+});
+
+io.on('connection', (socket) => {
+    socket.on('request_blogs', (id) => {
+        var dirFs = fs.readdirSync('./blogs');
+        var blogContents = [];
+    
+        for (let i = 0; i < dirFs.length; i++) {
+            if (dirFs[i].substring(dirFs[i].length - 5, dirFs[i].length) == '.json') {
+                fs.readFile('./blogs/' + dirFs[i], function (err, buf) {
+                    var cont = buf.toString();
+
+                    console.log(cont);
+                });
+            }
+        }
+    });
+});
+
+app.get('/blog', (req, res) => {
+    res.sendFile(__dirname + '/pages/blog.html');
+});
+
+app.get('/login', (req, res) => {
+    let qualified = req.query.password[1] == PSW;
+
+    if (qualified) {
+        const token = jwt.sign({ name: req.query.password[0] }, KEY, { expiresIn: 60 * 60 });
+        res.cookie('token', token, {
+            httpOnly: true,
+        });
+
+        return res.redirect('/');
+    }
 });
 
 app.listen(PORT, () => {
